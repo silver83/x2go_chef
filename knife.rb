49c8d5a6749c2af30d285ad6216394bc75ed5c40
Yoni Shalom <silver83@gmail.com>
2013-06-16 12:54:25 +0300

    some fixes

--- knife.rb
+++ knife.rb
@@ -1,2 +1,6 @@
 knife[:aws_access_key_id] = "#{ENV['AWS_ACCESS_KEY_ID']}"
 knife[:aws_secret_access_key] = "#{ENV['AWS_SECRET_ACCESS_KEY']}"
+knife[:aws_ssh_key_id] = "#{ENV['AWS_SSH_KEY_ID']}"
+
+
+puts "#{ENV['AWS_SSH_KEY_ID']}"
--- newserver.sh
+++ newserver.sh
@@ -1,4 +1,3 @@
 # provision a new m1.small Ubuntu 10.04 webserver
-knife ec2 server create -r 'role[webserver]' -I ami-1d620e74 -f m1.small -A 'Your AWS Access Key ID' -K "Your AWS Secret Access Key"
-
-knife solo prepare admin@ec2-54-226-159-138.compute-1.amazonaws.com
+knife ec2 server create -I ami-1d620e74 -G default -f t1.micro -x admin
+knife solo bootstrap admin@ec2-184-73-140-228.compute-1.amazonaws.com
